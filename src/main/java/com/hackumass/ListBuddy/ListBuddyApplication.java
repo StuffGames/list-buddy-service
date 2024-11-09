31ec1ff0b5846ea8680df1a92448b66a27c90330
StuffGames <44318828+StuffGames@users.noreply.github.com>
2024-11-09 20:51:47 +0000

    Initial SpringBoot Project Creation

---
+++ src/main/java/com/hackumass/ListBuddy/ListBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.hackumass.ListBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListBuddyApplication.class, args);
+	}
+
+}
